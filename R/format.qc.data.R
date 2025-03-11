17b4b6bcf0b4f4a2ea1e69f49c3ab07f37034ff9
siwei.wu <wusiwei2022@163.com>
2025-03-11 14:51:52 +0000

    update format.data

--- R/format.qc.data.R
+++ R/format.qc.data.R
@@ -1,33 +1,63 @@
 #' Convert a GWAS Summary data into a standard format for downstream colocalization analysis 
+#' @param data the data frame to be formatted, either data or data.path should be provided
+#' @param data.path path to the data frame to be formatted, either data or data.path should be provided
+#' @param TRAIT trait name
+#' @param RSID the ID column for genetic variants, must be provided 
+#' @param CHR column for variant chromosome
+#' @param POS column  for variant position
+#' @param EA column for the effect allele
+#' @param NEA column for the non-effect allele
+#' @param EAF column for the effect allele frequency
+#' @param MAF column for the minor allele frequency. If not available, MAF can be calculated from EAF
+#' @param BETA column for the beta in regression model
+#' @param SE column for the standard error of beta in regression model
+#' @param Z column for the Z statistics in regression model
+#' @param P column for the P value to test beta = 0 
+#' @param LOG10P column for the log10P to test beta = 0 
+#' @param N column for the sample size in GWAS
+#' @return A post-formated data frame
 #' @export format.data
 format.data = function(data = NULL, data.path = NULL, TRAIT, 
-                       RSID = "RSID", CHR = "CHR", POS = "POS", 
-                       EA = "EA", NEA = "NEA", EAF = NULL, MAF = NULL, 
-                       BETA = "BETA", SE = "SE", 
-                       P = NULL, LOG10P = NULL, N = NULL){
+                       RSID = "RSID", CHR = NA, POS = NA, 
+                       EA = NA, NEA = NA, EAF = NA, MAF = NA, 
+                       BETA = NA, SE = NA, Z = NA,
+                       P = NA, LOG10P = NA, N = NA){
   if(is.null(data) & is.null(data.path)){stop("please provide gwas summary data or path to gwas summary data")}
   if(is.null(data) & !is.null(data.path)){data = data.table::fread(data.path)}
+  if(is.na(RSID) | !{RSID %in% names(data)}){stop("please provide unique identifiers for genetic variants")}
   data = as.data.frame(data)
-  std.data = data.frame(rsid = data[, RSID],
-                        chr = data[, CHR],
-                        pos = data[, POS],
-                        ea = data[, EA],
-                        nea = data[, NEA],
-                        beta = data[, BETA],
-                        se = data[, SE])
+  std.data = data.frame(rsid = data[, RSID])
+  
   ### Trait name
   std.data = std.data %>% dplyr::mutate(trait = TRAIT, .before = "rsid")
-  ### P value and log10 P value
-  if(is.null(P)){std.data = std.data %>% dplyr::mutate(p = NULL)}else{std.data = std.data %>% dplyr::mutate(p = data[, P])}
-  if(is.null(LOG10P)){std.data = std.data %>% dplyr::mutate(log10p = NULL)}else{std.data = std.data %>% dplyr::mutate(log10p = data[, LOG10P])}
-  ### Sample size
-  if(is.null(N)){std.data = std.data %>% dplyr::mutate(n = NULL)}else{std.data = std.data %>% dplyr::mutate(n = data[, N])}
+  
+  ### Coordinate 
+  if(!is.na(CHR) & CHR %in% names(data)){std.data = std.data %>% dplyr::mutate(chr = data[, CHR])}else{std.data = std.data %>% dplyr::mutate(chr = NA)}
+  if(!is.na(POS) & POS %in% names(data)){std.data = std.data %>% dplyr::mutate(pos = data[, POS])}else{std.data = std.data %>% dplyr::mutate(pos = NA)}
+  
+  ### Effect allele and non-effect allele
+  if(!is.na(EA) & EA %in% names(data)){std.data = std.data %>% dplyr::mutate(ea = data[, EA])}else{std.data = std.data %>% dplyr::mutate(ea = NA)}
+  if(!is.na(NEA) & NEA %in% names(data)){std.data = std.data %>% dplyr::mutate(nea = data[, NEA])}else{std.data = std.data %>% dplyr::mutate(nea = NA)}
+  
   ### EAF
-  if(is.null(EAF)){std.data = std.data %>% dplyr::mutate(eaf = NULL)}else{std.data = std.data %>% dplyr::mutate(eaf = data[, EAF])}
+  if(!is.na(EAF) & EAF %in% names(data)){std.data = std.data %>% dplyr::mutate(eaf = data[, EAF])}else{std.data = std.data %>% dplyr::mutate(eaf = NA)}
   ### MAF
-  if(!is.null(MAF)){std.data = std.data %>% dplyr::mutate(maf = data[, MAF], .after = nea)}
-  if(is.null(MAF) & !is.null(EAF)){std.data = std.data %>% dplyr::mutate(maf = ifelse(eaf<0.5, eaf, 1-eaf), .after = nea)}
-  if(is.null(MAF) & is.null(EAF)){std.data = std.data %>% dplyr::mutate(maf = NULL, .after = nea)}
+  if(!is.na(MAF) & MAF %in% names(data)){std.data = std.data %>% dplyr::mutate(maf = data[, MAF], .after = nea)}
+  if(is.na(MAF) & !is.na(EAF) & EAF %in% names(data)){std.data = std.data %>% dplyr::mutate(maf = ifelse(eaf<0.5, eaf, 1-eaf))}
+  if(is.na(MAF) & {is.na(EAF) | !(EAF %in% names(data))}){std.data = std.data %>% dplyr::mutate(maf = NA)}
+  
+  ### Beta, Se, and Z statistics
+  if(!is.na(BETA) & BETA %in% names(data)){std.data = std.data %>% dplyr::mutate(beta = data[, BETA])}else{std.data = std.data %>% dplyr::mutate(beta = NA)}
+  if(!is.na(SE) & SE %in% names(data)){std.data = std.data %>% dplyr::mutate(se = data[, SE])}else{std.data = std.data %>% dplyr::mutate(se = NA)}
+  if(!is.na(Z) & Z %in% names(data)){std.data = std.data %>% dplyr::mutate(z = data[, Z])}else{std.data = std.data %>% dplyr::mutate(z = NA)}
+  
+  ### P value and log10 P value
+  if(!is.na(P) & P %in% names(data)){std.data = std.data %>% dplyr::mutate(p = data[, P])}else{std.data = std.data %>% dplyr::mutate(p = NA)}
+  if(!is.na(LOG10P) & LOG10P %in% names(data)){std.data = std.data %>% dplyr::mutate(log10p = data[, LOG10P])}else{std.data = std.data %>% dplyr::mutate(log10p = NA)}
+  
+  ### Sample size
+  if(!is.na(N) & N %in% names(data)){std.data = std.data %>% dplyr::mutate(n = data[, N])}else{std.data = std.data %>% dplyr::mutate(n = NA)}
+  
   return(std.data)
 }
 
